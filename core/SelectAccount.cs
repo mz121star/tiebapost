df2de78efb3956e48dc0b4a149a927f40290a02b
mz121star <mz121star@gmail.com>
2013-05-22 14:02:41 +0800

    add stylegenerator

---
+++ core/SelectAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using core;
+
+namespace baidutieba
+{
+    public partial class SelectAccount : Form
+    {
+        public SelectAccount()
+        {
+            InitializeComponent();
+        }
+
+        public LoginModel CurrentLogin = new LoginModel();
+        public SelectAccount(List<LoginModel> accounts)
+        {
+            InitializeComponent();
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = accounts;
+        }
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var login = (LoginModel)dataGridView1.Rows[e.RowIndex].DataBoundItem;
+            CurrentLogin = login;
+            this.DialogResult=DialogResult.OK;
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
